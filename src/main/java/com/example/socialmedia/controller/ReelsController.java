2dd7f89ecc1fd451d3940cb6c76297bb4054ff6f
BlessingGianna7 <blessinggianna7@gmail.com>
2024-08-15 08:00:29 +0200

    eleventh commit

--- src/main/java/com/example/socialmedia/controller/ReelsController.java
+++ src/main/java/com/example/socialmedia/controller/ReelsController.java
@@ -1,8 +1,12 @@
 package com.example.socialmedia.controller;
 
 
+import com.example.socialmedia.Service.ReelsService;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ReelsController {
+   private ReelsService reelsService
+
+
 }
