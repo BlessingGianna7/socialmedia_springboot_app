ba9d5bade10e486bcbf17c1578d4fbea3f2edd71
BlessingGianna7 <blessinggianna7@gmail.com>
2024-08-18 00:55:19 +0200

    eleventh commit

--- src/main/java/com/example/socialmedia/controller/ReelsController.java
+++ src/main/java/com/example/socialmedia/controller/ReelsController.java
@@ -1,12 +1,45 @@
 package com.example.socialmedia.controller;
 
 
+import com.example.socialmedia.Models.Reels;
+import com.example.socialmedia.Models.User;
 import com.example.socialmedia.Service.ReelsService;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.socialmedia.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ReelsController {
-   private ReelsService reelsService
+   @Autowired
+   private ReelsService reelsService;
+   @Autowired
+   private UserService userService;
+
+   @PostMapping("/api/reels")
+   public Reels createReels(@RequestBody Reels reel, @RequestHeader("Authorization") String jwt){
+
+    User reqUser =userService.findUserByJwt(jwt);
+      Reels createdReels = reelsService.createReel(reel,reqUser);
+   return  createdReels;
+   }
 
 
+   @GetMapping("/api/reels")
+   public Reels findAllReels(){
+
+      List<Reels> reels = reelsService.findAllReels();
+
+      return (Reels) reels;
+   }
+
+
+   @GetMapping("/api/reels/user/{userId}")
+   public Reels findUsersReels(@PathVariable Integer userId) throws Exception {
+
+      List<Reels> reels = reelsService.findUsersReel(userId);
+
+      return (Reels) reels;
+   }
 }
